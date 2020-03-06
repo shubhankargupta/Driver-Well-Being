87c8a14f927612708897914c3c002302dc813119
Shubhankar Gupta <shubhankargupta@campus-118-183.ucdavis.edu>
2020-03-06 12:05:42 -0800

    Added circular progressive bar

--- src/components/line-graph.component.js
+++ src/components/line-graph.component.js
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
 import {Chart} from "react-google-charts";
 
+//Import react-circular-progressbar module and styles
+import {
+  CircularProgressbar,
+  CircularProgressbarWithChildren,
+  buildStyles
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+
+// Animation
+import { easeQuadInOut } from "d3-ease";
+import AnimatedProgressProvider from "./AnimatedProgressProvider";
+import ChangingProgressProvider from "./ChangingProgressProvider";
+
+// Radial separators
+import RadialSeparators from "./RadialSeparators";
+
+
 export default class LineGraph extends Component
 {
+
+	constructor(props)
+	{
+		super(props);
+
+		this.state={
+			percentage : 49
+		}
+	}
+
 	render(){
 		return(
+			     <div>
                  <Chart
   width={'600px'}
   height={'400px'}
   chartType="LineChart"
   loader={<div>Loading Chart</div>}
   data={[
-    ['x', 'dogs'],
+    ['x', 'heart'],
     [0, 20],
     [1, 15],
     [2, 23],
@@ -30,12 +59,56 @@ export default class LineGraph extends Component
       title: 'Time',
     },
     vAxis: {
-      title: 'Heart-Beat',
+      title: 'Number of heart-beats',
     },
   }}
   rootProps={{ 'data-testid': '1' }}
 />
+   
+
+
+    
+    <Example label="percentage of steps completed">
+      <AnimatedProgressProvider
+        valueStart={0}
+        valueEnd={this.state.percentage}
+        duration={1.4}
+        easingFunction={easeQuadInOut}
+   
+      >
+        {value => {
+          const roundedValue = Math.round(value);
+          return (
+            <CircularProgressbar
+              value={value}
+              text={`${roundedValue}%`}
+              /* This is important to include, because if you're fully managing the
+        animation yourself, you'll want to disable the CSS animation. */
+              styles={buildStyles({ pathTransition: "none" })}
+            />
+          );
+        }}
+      </AnimatedProgressProvider>
+    </Example>
+
+
+</div>
+
 
 			)
 	}
+}
+
+function Example(props) {
+  return (
+    <div style={{ marginBottom: 80 }}>
+      <div style={{ marginTop: 30, display: "flex" }}>
+        <div style={{ width: "30%", paddingRight: 30 }}>{props.children}</div>
+        <div style={{ width: "70%" }}>
+          <h3 className="h5">{props.label}</h3>
+          <p>{props.description}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
